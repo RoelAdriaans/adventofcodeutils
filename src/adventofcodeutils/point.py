4d1c81bfab5282841c3621abc5e909fda382a3fe
Roel Adriaans <roel@adriaans.org>
2023-11-21 22:12:42 +0100

    Make points hashable and indexable via point.x and point["x"]

--- src/adventofcodeutils/point.py
+++ src/adventofcodeutils/point.py
@@ -12,6 +12,12 @@ class XYZPoint:
             raise ValueError("Cannot add incompatible types together")
         return XYZPoint(x=self.x + other.x, y=self.y + other.y, z=self.z + other.z)
 
+    def __getitem__(self, key):
+        return self.__getattribute__(key)
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
+
     def distance(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y) + abs(self.z - other.z)
 
@@ -25,6 +31,12 @@ class XYPoint:
         if not issubclass(type(other), XYPoint):
             raise ValueError("Cannot add incompatible types together")
         return XYPoint(x=self.x + other.x, y=self.y + other.y)
+
+    def __getitem__(self, key):
+        return self.__getattribute__(key)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
 
     def distance(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y)
